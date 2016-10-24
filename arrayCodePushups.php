daffbb0980bbb2313947d8ac131bcf7ed1a7e001
bchok <bchok91@gmail.com>
2016-10-24 13:30:41 -0400

    added search function

--- arrayCodePushups.php
+++ arrayCodePushups.php
@@ -9,4 +9,10 @@
         $array = array("color" => array("blue", "red", "green"), "size" => array("small", "medium", "large"));
         print_r(array_keys($array));
     }
+    
+    function arraySearch(){
+        $array = array(0=> 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+        $key = array_search('green', $array);
+        $key = array_search('red', $array);
+    }
 ?>
