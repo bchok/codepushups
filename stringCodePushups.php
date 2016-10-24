8ab0d3174d39c3723667365174402a866a3b24b3
bchok <bchok91@gmail.com>
2016-10-24 14:45:28 -0400

    added md5 function

--- stringCodePushups.php
+++ stringCodePushups.php
@@ -67,5 +67,12 @@
         var_dump($clean);
         
     }
+    
+    function stringMD5(){
+        $str = 'apple';
+        if(md5($str) === '1f3870be274f6c49b3e31a0c6728957f'){
+            echo "Would you like a green or red apple?";
+        }
+    }
         
 ?>
