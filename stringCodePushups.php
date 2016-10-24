5fd4428c6a30999f66abc07655b612aab6104752
bchok <bchok91@gmail.com>
2016-10-24 14:43:41 -0400

    added ltrim function

--- stringCodePushups.php
+++ stringCodePushups.php
@@ -45,4 +45,27 @@
         $bar = lcfirst($bar); //hELLO WORLD!
         $bar = lcfirst(strtoupper($bar)); // hELLO WORLD!
     }
+    
+    function stringLTrim(){
+        $text = "\t\tThese are a few words :)...";
+        $binary = "\x09Example string\x0A";
+        $hello = "Hello World";
+        var_dump($text, $binary, $hello);
+        
+        print "\n";
+        
+        $trimmed = ltrim($text);
+        var_dump($trimmed);
+        
+        $trimmed = ltrim($text, "\t.");
+        var_dump($trimmed);
+        
+        $trimmed = ltrim($hello, "Hdle");
+        var_dump($trimmed);
+        
+        $clean = ltrim($binary, "\x00..\x1F");
+        var_dump($clean);
+        
+    }
+        
 ?>
