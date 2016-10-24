0f9cc78d7b9f1b4e7590095955295ab828faa614
bchok <bchok91@gmail.com>
2016-10-24 13:34:18 -0400

    added array walk function

--- arrayCodePushups.php
+++ arrayCodePushups.php
@@ -15,4 +15,24 @@
         $key = array_search('green', $array);
         $key = array_search('red', $array);
     }
+    
+    function arrayWalk(){
+            $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+
+            function test_alter(&$item1, $key, $prefix){
+                $item1 = "$prefix: $item1";
+            }
+
+            function test_print($item2, $key){
+                echo "$key. $item2<br />\n";
+            }
+
+            echo "Before ...:\n";
+            array_walk($fruits, 'test_print');
+
+            array_walk($fruits, 'test_alter', 'fruit');
+            echo "... and after:\n";
+
+            array_walk($fruits, 'test_print');
+   }  
 ?>
