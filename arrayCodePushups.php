d5df35de46ac9de9a12012aefa4c7859dd6f24fe
bchok <bchok91@gmail.com>
2016-10-24 14:17:52 -0400

    added array rand functions

--- arrayCodePushups.php
+++ arrayCodePushups.php
@@ -119,4 +119,11 @@
        $a = array_fill_keys($keys, 'banana');
        print_r($a);
    }
+   
+   function arrayRand(){
+       $input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+       $rand_keys = array_rand($input, 2);
+       echo $input[$rand_keys[0]] . "\n";
+       echo $input[$rand_keys[1]] . "\n";
+   }
 ?>
