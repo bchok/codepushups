dddbfdc2e203f44cfcc6800d0121f59d3c790f89
bchok <bchok91@gmail.com>
2016-10-24 14:24:58 -0400

    added count chars function

--- stringCodePushups.php
+++ stringCodePushups.php
@@ -4,4 +4,12 @@
         //format $data using RFC 2045 semantics
         $new_string = chunk_split(base64_encode($data));
     }
+    
+    function countChars(){
+        $data = "Two Ts and one F.";
+        
+        foreach (count_chars($data, 1) as $i => $val){
+            echo "There were $val instance(s) of \"", chr($i), "\" in the string.\n";
+        }
+    }
 ?>
