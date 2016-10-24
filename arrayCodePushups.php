18a0c4f6ff427776b39ebf6dcf3e21fd33dbb17e
bchok <bchok91@gmail.com>
2016-10-24 14:05:25 -0400

    added array diff function

--- arrayCodePushups.php
+++ arrayCodePushups.php
@@ -94,4 +94,12 @@
        $result = array_intersect($array1, $array2);
        print_r($result);
    }
+   
+   function arrayDiff(){
+       $array1 = array("a" => "green", "red", "blue");
+       $array2 = array("b" => "green", "yellow", "red");
+       $result = array_diff($array1, $array2);
+       
+       print_r($result);
+   }
 ?>
